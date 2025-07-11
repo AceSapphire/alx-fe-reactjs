e9b6a804da123815a18a34edea422a6a0e178005
Rachel MacMathew <rachmacmathew@gmail.com>
2025-07-11 23:44:40 +0000

    Added a Header, Maincontent and Footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,17 @@
-import WelcomeMessage from "./components/WelcomeMessage"
+// import WelcomeMessage from "./components/WelcomeMessage"
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import React from 'react'
 
 const App = () => {
   return (
-    <WelcomeMessage />
+    // <WelcomeMessage />
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
   )
 }
 
