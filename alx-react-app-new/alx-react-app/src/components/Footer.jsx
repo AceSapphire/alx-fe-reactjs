99d32aa8a8aca983d5c24c28a723de75dd5c7b78
Rachel MacMathew <rachmacmathew@gmail.com>
2025-07-20 15:42:28 +0000

    Add inline styling to Header, UserProfile, MainContent, and Footer

---
+++ alx-react-app-new/alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#333', color: 'white', textAlign: 'center', padding: '15px' }}>
+      <p>© 2025 My Travel Site. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
