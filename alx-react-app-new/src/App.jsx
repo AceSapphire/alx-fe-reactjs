20e9a31a72a6705ca117b67ca8d1acdc63c2fd1e
Rachel MacMathew <rachmacmathew@gmail.com>
2025-07-20 16:49:33 +0000

    Add Counter component with useState for increment, decrement, and reset

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,15 +3,16 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import React from 'react'
+import Counter from './components/Counter';
 
-const App = () => {
+function App() {
   return (
     // <WelcomeMessage />
     <div>
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
       <Footer />
     </div>
   )
