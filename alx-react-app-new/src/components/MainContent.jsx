615382318f2c732e2585f845ecffd757a7a1b224
Rachel MacMathew <rachmacmathew@gmail.com>
2025-07-20 16:33:32 +0000

    Add inline CSS styling to Header, UserProfile, MainContent, and Footer components

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f0f0f0' }}>
+      <p>Welcome to my favorite cities app!</p>
+    </main>
+  );
+}
+
+export default MainContent;
