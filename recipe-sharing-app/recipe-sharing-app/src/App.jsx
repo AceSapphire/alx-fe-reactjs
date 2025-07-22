324bdb279881eaab429ec8b7ba52d7b88b83fe50
Rachel MacMathew <rachmacmathew@gmail.com>
2025-07-22 23:25:14 +0000

    started learning Zustand

---
+++ recipe-sharing-app/recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import RecipeList from "./Components/RecipeList"
+import AddRecipeForm from "./Components/AddRecipeForm"
+
+const App = () => {
+  return (
+    <div>
+      <h1>App Page</h1>
+      <RecipeList />
+      <AddRecipeForm />
+    </div>
+  )
+}
+
+export default App
